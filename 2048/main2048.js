e50b4d82b8cffeff9a3bea48ba30d1507d196e64
acwong <acwong@live.cn>
2014-10-30 14:37:27 +0800

    finish four directions

--- 2048/main2048.js
+++ 2048/main2048.js
@@ -125,7 +125,7 @@ function moveLeft() {
         for (var j = 1; j < 4; j++) {
             if (board[i][j] !== 0) {
                 for (var k = 0; k < j; k++) {
-                    if (board[i][k] == 0 && noBlockHorizontal(i,k,j,board)) {
+                    if (board[i][k] === 0 && noBlockHorizontal(i,k,j,board)) {
                         showMoveAnimation(i,j,i,k);
                         board[i][k] = board[i][j];
                         board[i][j] = 0;
@@ -151,8 +151,76 @@ function moveUp() {
     for (var i = 1; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             if (board[i][j] !== 0) {
-                for
+                for (var k = 0; k < i; k++) {
+                    if (board[k][j] === 0 && noBlockVertical(j,k,i,board)) {
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] = board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    } else if (board[k][j] === board[i][j] && noBlockVertical(j,k,i,board)) {
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] += board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    }
+                }
             }
         }
     }
+    setTimeout(updateBoardView,200);
+    return true;
+}
+
+function moveRight() {
+    if (!canMoveRight(board)) {
+        return false;
+    }
+    for (var i = 0; i < 4; i++) {
+        for (var j = 2; j >= 0; j--) {
+            if (board[i][j] !== 0) {
+                for (var k = 3; j < k; k--) {
+                    if (board[i][k] === 0 && noBlockHorizontal(i, k, j, board)) {
+                        showMoveAnimation(i, j, i, k);
+                        board[i][k] = board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    } else if (board[i][k] === board[i][j] && noBlockHorizontal(i, k, j, board)) {
+                        showMoveAnimation(i, j, i, k);
+                        board[i][k] += board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout(updateBoardView, 200);
+    return true;
+}
+
+function moveDown () {
+    if (!canMoveDown(board)) {
+        return false;
+    }
+    for (var i = 3; i >= 0; i--) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j] !== 0) {
+                for (var k = 3; j < k; k--) {
+                    if (board[k][j] === 0 && noBlockVertical(j,k,i,board)) {
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] = board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    } else if (board[k][j] === board[i][j] && noBlockVertical(j,k,i,board)) {
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] += board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout(updateBoardView,200);
+    return true;
 }
--- 2048/support2048.js
+++ 2048/support2048.js
@@ -70,9 +70,43 @@ function canMoveUp (board) {
     return false;
 }
 
+function canMoveRight (board) {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (board[i][j] !== 0) {
+                if (board[i][j+1] === 0 || board[i][j+1] === board[i][j]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function canMoveDown (board) {
+    for (var i = 1; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j] !== 0) {
+                if (board[i+1][j] === 0 || board[i+1][j] === board[i][j]) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+
 function noBlockHorizontal(row,col1,col2,board) {
     for (var i = col1 + 1; i < col2; i++) {
-        if (board[row][i] != 0) {
+        if (board[row][i] !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function noBlockVertical(col,row1,row2,board) {
+    for (var i = row1; i < row2; i++) {
+        if (board[i][col] !== 0) {
             return false;
         }
     }
