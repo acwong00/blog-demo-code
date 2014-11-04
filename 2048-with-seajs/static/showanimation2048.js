ae201cf2f61c5bbcd40832b8046672bf5f6e272e
acwong <acwong@live.cn>
2014-11-04 14:23:18 +0800

    create 2048 with seajs

---
+++ 2048-with-seajs/static/showanimation2048.js
@@ -0,0 +1,31 @@
+define(function(require,exports,moudule) {
+    var $ = require('jquery');
+    var support = require('./support2048')
+
+    exports.showNumberWithAnimation = function(i, j, randNumber) {
+        var numberCell = $('#number-cell-' + i + '-' + j);
+        numberCell.css({
+            'background-color': support.getNumberBackgroundColor(randNumber),
+            'color': support.getNumberColor(randNumber)
+        }).text(randNumber);
+
+        numberCell.animate({
+            'width': support.cellSideLength,
+            'height': support.cellSideLength,
+            'top': support.getPostTop(i, j),
+            'left': support.getPostLeft(i, j)
+        }, 100);
+    };
+
+    exports.showMoveAnimation = function(fromx, fromy, tox, toy) {
+        var numberCell = $('#number-cell-' + fromx + '-' + fromy);
+        numberCell.animate({
+            'top': support.getPostTop(tox, toy),
+            'left': support.getPostLeft(tox, toy)
+        }, 200);
+    };
+
+    exports.updateScore = function(score) {
+        $('#score').text(score);
+    }
+});
